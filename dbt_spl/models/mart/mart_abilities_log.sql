63ee4bde8a7a606ffadb6322dd780a87a45ea000
chura-ytb <y.tobi@churadata.okinawa>
2025-05-09 15:35:34 +0900

    mart以下のファイル修正

--- dbt_spl/models/mart/mart_abilities_log.sql
+++ dbt_spl/models/mart/mart_abilities_log.sql
@@ -1,2 +1,2 @@
 select *
-from {{ref('int_abilities_log')}}
+from {{ ref('int_abilities_log') }}
--- dbt_spl/models/mart/mart_kill_rate_and_abilities.sql
+++ dbt_spl/models/mart/mart_kill_rate_and_abilities.sql
@@ -1,8 +1,10 @@
-select 
-    a.*,  
-    b.ability_name,  
-    b.ability_value 
-from {{ref('int_kill_rate')}} as a
-left join {{ref('int_abilities_log')}} as b
-on  a.battle_id = b.battle_id
-and a.member_id = b.member_id
+select
+    a.*,
+    b.ability_name,
+    b.ability_value
+from {{ ref('int_kill_rate') }} as a
+left join {{ ref('int_abilities_log') }} as b
+    on
+        a.battle_id = b.battle_id
+        and
+        a.member_id = b.member_id
